b3b2ec9b8c8bfed73941438ed67ff190a87ffb06
Bhagi <puppyreddy1997@gmail.com>
2020-10-02 20:18:28 +0530

    [Bhagyalaxmi]Added:created a contactperson

--- src/main/java/com/addressbook/AddressBook.java
+++ src/main/java/com/addressbook/AddressBook.java
@@ -1,8 +1,27 @@
 package com.addressbook;
 
 public class AddressBook {
-    public static void main(String args[])
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    long zip;
+    long phoneNumber;
+    public AddressBook(String firstName,String lastName,String address,String city,String state,long zip,long phoneNumber)
+    {
+     this.firstName = firstName;
+     this.lastName = lastName;
+     this.address = address;
+     this.city = city;
+     this.state = state;
+     this.zip = zip;
+     this.phoneNumber = phoneNumber;
+    }
+    public static void main(String[] args)
     {
         System.out.println("Welcome to addressbook");
+        
     }
+
 }
